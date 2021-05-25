57ca4acedb3efecd580623602cf67d3bf42e1ca0
alemdar1 <alemdarsu@gmail.com>
2021-05-25 19:23:37 -0400

    initial commit

---
+++ src/test/java/BranchingPractise.java
@@ -0,0 +1,7 @@
+public class BranchingPractise {
+    public static void main(String[] args) {
+        //created new project
+        //enable version control
+        //added .gitignore file with .idea,target folder and .imll
+    }
+}
